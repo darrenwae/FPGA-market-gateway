439541c7e3a3b55f14bb10992f21531ef8ca4070
Darren <darrenweyyh@gmail.com>
2026-07-16 18:15:35 +0800

    add RX block lock and descrambler

--- fpga/rtl/pcs/pcs_rx_descrambler.sv
+++ fpga/rtl/pcs/pcs_rx_descrambler.sv
@@ -1,33 +1,59 @@
-module pcs_rx_descrambler #() (
-    input logic [63:0] pcs_rx_scrambled_block_payload_in, // 64b/66b aligned block    
-    input logic [1:0]  pcs_rx_header_in, // 2-bit sync header
-    input logic pcs_rx_block_valid_in,
-    input logic clk, rst,
-    output logic [63:0] pcs_rx_descrambled_block_payload_out, //64 bits descrambled data
-    output logic [1:0] pcs_rx_header_out, //pass through header
-    output logic pcs_rx_header_valid_out
+// implements self synchronising 64/66b payload descrambler
+
+module pcs_rx_descrambler (
+    input logic clk,
+    input logic rst,
+
+    input logic block_lock,
+    input logic [63:0] block_payload,
+    input logic [1:0] block_header,
+    input logic block_valid,
+
+    output logic [63:0] descrambled_payload,
+    output logic [1:0] header_out,
+    output logic descrambled_payload_valid
 );
 
-    logic [57:0] Rx_lfsr;
-    logic [63:0] descrambled_data_out;
+    //rx_lfsr[57] = scrambled bit received 58 bit-times ago
+    //rx_lfsr[0]  = most recently received scrambled bit
+    logic [57:0] rx_lfsr;
+    logic sync_pending;
+    logic [63:0] descrambled_payload_next;
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            Rx_lfsr <= 58'b0;
+            rx_lfsr <= '0;
+            sync_pending <= 1'b1;
+            descrambled_payload <= '0;
+            header_out <= '0;
+            descrambled_payload_valid <= 1'b0;
         end
         else begin
-            if (pcs_rx_block_valid_in) begin
-                Rx_lfsr <= pcs_rx_scrambled_block_payload_in[57:0];
+            descrambled_payload_valid <= 1'b0;
+            if (!block_lock) begin
+                // alignment lost, have to re-sync
+                rx_lfsr <= '0;
+                sync_pending <= 1'b1;
             end
-            pcs_rx_header_out <= pcs_rx_header_in;
-            pcs_rx_header_valid_out <= pcs_rx_block_valid_in;
-            pcs_rx_descrambled_block_payload_out <= descrambled_data_out;
+            else if (block_valid) begin
+                // last 58 bits used as lfsr for descrambling next block
+                rx_lfsr <= block_payload[57:0];
+                if (sync_pending) begin
+                    // first aligned block used for synchronising lfsr with transmitter's lfsr
+                    sync_pending <= 1'b0;
+                end
+                else begin
+                    descrambled_payload <= descrambled_payload_next;
+                    header_out <= block_header;
+                    descrambled_payload_valid <= 1'b1;
+                end
+            end
         end
     end
 
     always_comb begin
         for (int j = 0; j < 64; j++) begin
-            descrambled_data_out[j] = pcs_rx_scrambled_block_payload_in[j] ^ ((j <= 24) ? pcs_rx_scrambled_block_payload_in[j+39] : Rx_lfsr[j-25]) ^ ((j <= 5) ? pcs_rx_scrambled_block_payload_in[j+58] : Rx_lfsr[j-6]);
+            descrambled_payload_next[j] = block_payload[j] ^ ((j <= 24) ? block_payload[j + 39] : rx_lfsr[j - 25]) ^ ((j <= 5) ? block_payload[j + 58] : rx_lfsr[j - 6]);
         end
     end
 
