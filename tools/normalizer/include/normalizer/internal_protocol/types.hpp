d4072cb2613cbab834e181497b92617de0886c63
Darren <darrenweyyh@gmail.com>
2026-08-05 20:56:31 +0800

    added upstream order decision types needed

--- tools/normalizer/include/normalizer/internal_protocol/types.hpp
+++ tools/normalizer/include/normalizer/internal_protocol/types.hpp
@@ -1,9 +1,7 @@
 #pragma once
-#include <cstdint>
-#include <cstddef>
 #include <array>
-
-
+#include <cstddef>
+#include <cstdint>
 
 namespace normalizer::internal_protocol {
 
@@ -30,13 +28,13 @@ namespace normalizer::internal_protocol {
     inline constexpr std::size_t Payload3Offset = 28;
     inline constexpr std::size_t PayloadWordSize = 4;
 
-
     enum class MessageType : std::uint8_t {
         SessionStatus = 1,
         TopOfBookUpdate = 2,
         SymbolStatus = 3,
         OrderIntent = 4,
-        ConfigControl = 5
+        ConfigControl = 5,
+        OrderDecision = 0x81
     };
 
     enum class SessionState : std::uint32_t {
@@ -56,5 +54,36 @@ namespace normalizer::internal_protocol {
         QuotationOnly = 3,
         Trading = 4,
     };
-    
-}
+
+    enum class Decision : std::uint32_t {
+        UnknownInvalid = 0,
+        Accept = 1,
+        Reject = 2,
+    };
+
+    enum class RejectReason : std::uint32_t {
+        None = 0,
+        StreamFault = 1,
+        OrderIntentProtocolViolation = 2,
+        UnknownSymbol = 3,
+        SymbolDisabled = 4,
+        SymbolNotTrading = 5,
+        RequiredTobSideInvalid = 6,
+        InvalidOrderSide = 7,
+        ZeroOrderQuantity = 8,
+        MaxOrderQuantityExceeded = 9,
+        ZeroOrderPrice = 10,
+        PriceBandViolation = 11,
+        MaxNotionalExceeded = 12,
+        SessionNotTrading = 13,
+    };
+
+    struct OrderDecision {
+        std::uint32_t sequenceNumber{0};
+        Decision decision{Decision::UnknownInvalid};
+        RejectReason rejectReason{RejectReason::None};
+        std::uint32_t intentId{0};
+        std::uint16_t symbolId{0};
+    };
+
+} // namespace normalizer::internal_protocol
